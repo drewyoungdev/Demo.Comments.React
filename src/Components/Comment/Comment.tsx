b95a453298ad70d1c87a1dff37a150388c8bde8e
Andrew Young <young.andrew2@outlook.com>
2019-10-15 20:50:34 -0400

    Adding comment padding adjustment

--- src/Components/Comment/Comment.tsx
+++ src/Components/Comment/Comment.tsx
@@ -6,10 +6,26 @@ interface CommentProps {
     comment: CommentModel;
 }
 
+const calcPaddingLeftPx = (depth: number) => {
+    const threadWidthPx = 16;
+    const threadMarginLeftPx = 5;
+    const baselinePaddingLeftPx = threadWidthPx + threadMarginLeftPx;
+
+    const additionalPaddingLeftPx = 25;
+
+    if (depth === 0) {
+        return `${baselinePaddingLeftPx}px`;
+    }
+
+    return `${(baselinePaddingLeftPx * depth) + additionalPaddingLeftPx}px`
+}
+
 const Comment: React.FC<CommentProps> = (props) => {
     return (
-        <div className="comment">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor risus sit amet neque fringilla, vitae ultrices libero egestas.
+        <div style={{ paddingLeft: calcPaddingLeftPx(props.comment.depth)}}>
+            <div className="comment">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor risus sit amet neque fringilla, vitae ultrices libero egestas.
+            </div>
         </div>
     );
 }
